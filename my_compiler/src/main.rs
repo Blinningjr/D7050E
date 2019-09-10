bcb9b8fcec6c08d283d19c33af7d2e0303d010dd
Blinningjr <nicke.l@telia.com>
2019-09-10 13:58:56 +0200

    added test for let

--- my_compiler/src/main.rs
+++ my_compiler/src/main.rs
@@ -4,8 +4,9 @@ pub use crate::parser::parse_expr;
 pub use crate::parser::math_expr_eval;
 
 fn main() {
-    println!("{:?}", parse_expr("let apa = 20"));
-    println!("{:?}", math_expr_eval(parse_expr("10 / 2 * 3 - 2 + 4 % 6").unwrap().1));
+    println!("{:?}", parse_expr(" 1 + 2 let apa = 20"));
+    let v = parse_expr("1 + 2 let apa = 20").unwrap();
+    println!("{:?} : {:?}" , v.0, math_expr_eval(v.1));
 
     println!("{:?}", 2-15+11);
 }
