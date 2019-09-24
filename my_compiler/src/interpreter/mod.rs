3214dd15745ba61c10d818bd353a417bf535e3fc
Blinningjr <nicke.l@telia.com>
2019-09-24 11:15:21 +0200

    interp assert half done

--- my_compiler/src/interpreter/mod.rs
+++ my_compiler/src/interpreter/mod.rs
@@ -53,7 +53,7 @@ use std::collections::HashMap;
 */
 #[derive(Debug, PartialEq, Clone)]
 pub struct Env<'a> {
-    mem: HashMap<&'a str, Expr<'a>>,
+    mem: HashMap<&'a str, Val>,
 }
 impl<'a> Env<'a> {
     pub fn new() -> Env<'a> {
@@ -61,16 +61,20 @@ impl<'a> Env<'a> {
             mem: HashMap::new(),
         }
     }
+    pub fn store(&mut self, ident: &'a str, val: Val) {
+        self.mem.insert(ident, val);
+    }
 }
 
 
 /** 
  *  Defins Val so bool and i32 can be returnd.
 */
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Val {
     Num(i32),
     Bool(bool),
+    Empty,
 }
 
 
@@ -78,19 +82,23 @@ enum Val {
  *  Interprets a ast.
 */
 pub fn interp_ast(e: Expr) -> () {
-    println!("{:?}", interp_expr(e, Env::new()));
+    let mut env = Env::new();
+    println!("{:?}", interp_expr(e, &env));
+    env.store("test", Val::Empty);
+    println!("{:?}", env);
 }
 
 
 /** 
  *  Interprets expresions in ast.
 */
-fn interp_expr(e: Expr, env: Env) -> Result<Val> {
+fn interp_expr(e: Expr, env: &Env) -> Result<Val> {
     match e {
         Expr::Num(i) => Ok(Val::Num(i)),
         Expr::Bool(i) => Ok(Val::Bool(i)),
-        Expr::UnOp(op, rv) => Ok(interp_unop(op, *rv, env.clone()).unwrap()),
-        Expr::BinOp(lv, op, rv) => Ok(interp_binop(*lv, op, *rv, env.clone()).unwrap()),
+        Expr::UnOp(op, rv) => Ok(interp_unop(op, *rv, &env).unwrap()),
+        Expr::BinOp(lv, op, rv) => Ok(interp_binop(*lv, op, *rv, &env).unwrap()),
+        Expr::Assign(i, v) => interp_assign(*i, *v, &env),
         _ => Err(InterpError),
     }
 }
@@ -99,17 +107,17 @@ fn interp_expr(e: Expr, env: Env) -> Result<Val> {
 /** 
  *  Interprets unary operations in ast.
 */
-fn interp_unop(op: Op, e: Expr, env: Env) -> Result<Val> {
+fn interp_unop(op: Op, e: Expr, env: &Env) -> Result<Val> {
     match op {
         Op::Sub => {
-            let res = interp_expr(e, env).unwrap();
+            let res = interp_expr(e, &env).unwrap();
             match res {
                 Val::Num(i) => Ok(Val::Num(-i)),
                 _ => Err(InterpError),
             }
         }
         Op::Not => {
-            let res = interp_expr(e, env).unwrap();
+            let res = interp_expr(e, &env).unwrap();
             match res {
                 Val::Bool(b) => Ok(Val::Bool(!b)),
                 _ => Err(InterpError),
@@ -123,72 +131,72 @@ fn interp_unop(op: Op, e: Expr, env: Env) -> Result<Val> {
 /** 
  *  Interprets binary operations in ast.
 */
-fn interp_binop(lv: Expr, op: Op, rv: Expr, env: Env) -> Result<Val> {
+fn interp_binop(lv: Expr, op: Op, rv: Expr, env: &Env) -> Result<Val> {
     match op {
         Op::Add => Ok(Val::Num(
-            get_int(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(lv, &env).unwrap()).unwrap()
             +
-            get_int(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::Sub => Ok(Val::Num(
-            get_int(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(lv, &env).unwrap()).unwrap()
             -
-            get_int(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::Div => Ok(Val::Num(
-            get_int(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(lv, &env).unwrap()).unwrap()
             /
-            get_int(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::Multi => Ok(Val::Num(
-            get_int(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(lv, &env).unwrap()).unwrap()
             *
-            get_int(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::Mod => Ok(Val::Num(
-            get_int(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(lv, &env).unwrap()).unwrap()
             %
-            get_int(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::LessEqThen => Ok(Val::Bool(
-            get_int(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(lv, &env).unwrap()).unwrap()
             <=
-            get_int(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::LargEqThen => Ok(Val::Bool(
-            get_int(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(lv, &env).unwrap()).unwrap()
             >=
-            get_int(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::LessThen => Ok(Val::Bool(
-            get_int(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(lv, &env).unwrap()).unwrap()
             <
-            get_int(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::LargThen => Ok(Val::Bool(
-            get_int(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(lv, &env).unwrap()).unwrap()
             >
-            get_int(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::Equal => Ok(Val::Bool(
-            get_int(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(lv, &env).unwrap()).unwrap()
             ==
-            get_int(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_int(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::And => Ok(Val::Bool(
-            get_bool(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_bool(interp_expr(lv, &env).unwrap()).unwrap()
             &&
-            get_bool(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_bool(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::Or => Ok(Val::Bool(
-            get_bool(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_bool(interp_expr(lv, &env).unwrap()).unwrap()
             ||
-            get_bool(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_bool(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         Op::NotEq => Ok(Val::Bool(
-            get_bool(interp_expr(lv, env.clone()).unwrap()).unwrap()
+            get_bool(interp_expr(lv, &env).unwrap()).unwrap()
             !=
-            get_bool(interp_expr(rv, env.clone()).unwrap()).unwrap()
+            get_bool(interp_expr(rv, &env).unwrap()).unwrap()
         )),
         _ => Err(InterpError),
     }
@@ -213,5 +221,22 @@ fn get_bool(v: Val) -> Result<bool> {
     match v {
         Val::Bool(b) => Ok(b),
         _ => Err(InterpError),
+    }
+}
+
+
+
+fn interp_assign(ident: Expr, value: Expr, env: &Env) -> Result<Val> {
+    match ident {
+        Expr::Assign(i, _t) =>{
+            let val = interp_expr(value, &env).unwrap();
+            // env.store(&i.to_string(), val.clone());
+            return Ok(val);
+        },
+        _ => {
+            let val = interp_expr(value, &env).unwrap();
+            // env.store("test", val.clone());
+            return Ok(val);
+        },
     }
 }
--- my_compiler/src/main.rs
+++ my_compiler/src/main.rs
@@ -11,7 +11,7 @@ pub use crate::interpreter::interp_ast;
 
 fn main() {
     // println!("{:#?}", parse_funcs("fn apor(input: Str, test) -> bool { let apa = 10; while apa >= 2 { let apa = 10 /2 * 5 -2;}} fn test(inp) -> None { if inp == false { let inp = true; } else { let inp = false;} if inp { let apa = 5 % 2; }}")); 
-    let v = parse_expr("  11 > 10").unwrap();
+    let v = parse_expr("let apa = true;").unwrap();
     println!("{:?} : {:?}" , v.0, interp_ast(v.1));
 
     // println!("{:#?}",  parse_funcs("fn test(inp) -> None { if inp == false { let inp = true; } else { let inp = false;} if inp { let apa = 5 % 2; }}"));
